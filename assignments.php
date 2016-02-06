a0f0ae44299f100d69015ab2d8279129987b327e
Jared <bea12052@byui.edu>
2016-02-06 14:05:31 -0700

    Adding new home page and movie manager

--- assignments.php
+++ assignments.php
@@ -1,23 +1,49 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
-		<meta charset="utf-8">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script src="home.js"></script>
-		<link rel="stylesheet" type="text/css" href="home.css" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+		<title>Assignments</title>
+		<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script src="js/materialize.js"></script>
+		<script src="js/init.js"></script>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+		<link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 	</head>
 	<body>
-		<div id="headerBar">
-			<div id="nameText">JARED BEAGLEY</div>
-		</div>
-		<div id="navigators">
-			<a href="index.php"><div id="nav1" class="topNav">HOME</div></a>
-			<a href="assignments.php"><div id="nav2" class="topNav">ASSIGNMENTS</div></a>
-		</div>
-		<div id="content">
-			<div id="assignments">
-				<a href="http://php-jbeagley.rhcloud.com/survey.php">PHP Survey</a>
+		<nav class="aub" role="navigation">
+			<div class="nav-wrapper container" >
+				<a id="logo-container" href="index.php" class="brand-logo"><span class="whiteT">JARED BEAGLEY</span></a>
+				<ul class="right hide-on-med-and-down" >
+					<li><a href="assignments.php" class="whiteT">Assignments</a></li>
+					<li><a href="index.php" class="whiteT">About Me</a></li>
+				</ul>	
+				<ul id="nav-mobile" class="side-nav">
+					<li><a href="assignments.php">Assignments</a></li>
+					<li><a href="index.php">About Me</a></li>
+				</ul>
+				<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons white">menu</i></a>
+			</div>
+		</nav>
+		<div class="section no-pad-bot" id="index-banner">
+			<div class="container">
+				<br><br>
+				<i class="material-icons md-100 aub-text">work</i>
+				<br><br>
+				<div class="assignList">
+					<p><a href="http://php-jbeagley.rhcloud.com/survey.php">PHP Survey</a></p>
+					<!--<p><a href="http://php-jbeagley.rhcloud.com/movies.php">Movie Manager</a></p>-->
+					<p><a href="movies.php">Movie Manager</a></p>
+				</div>
 			</div>
 		</div>
+		<footer class="page-footer aub">
+			<div class="footer-copyright">
+				<div class="container up">
+					<i class="material-icons md-15">copyright</i>&nbsp; 2016&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Made by Jared Beagley
+				</div>
+			</div>
+		</footer>
 	</body>
 </html>
